f46b2067306015139f5ece49e0750f652d25abd9
Darbados <pesho.netev@gmail.com>
2017-12-19 21:35:00 +0200

    Add files via upload

--- delasport/bowlingREST/admin.py
+++ delasport/bowlingREST/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Game
 
-# Register your models here.
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id','total_score','player','roll1','roll2','roll3','roll4','roll5','roll6','roll7','roll8','roll9','roll10','roll11','roll2','roll13','roll14','roll15','roll16','roll17','roll18','roll19','roll20','roll21')
+
+admin.site.register(Game, GameAdmin)
