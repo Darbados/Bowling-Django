dcf98a7647f4ec19969f13e122f64bf0581e188d
petar.n <petar.n@delasport.com>
2018-03-23 10:55:16 +0200

    adding proxies_service app to the delasport project

---
+++ delasport/proxies_service/models.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+# Here will be the source_proxies table represented as a django model, which will be not managed from django
