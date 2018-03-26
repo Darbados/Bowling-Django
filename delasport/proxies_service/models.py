6f2f877500bd7184bce1062aae9e9d48a4bb3325
petar.n <petar.n@delasport.com>
2018-03-26 09:36:05 +0300

    push them all

--- delasport/proxies_service/models.py
+++ delasport/proxies_service/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 # Here will be the source_proxies table represented as a django model, which will be not managed from django
 
-"""
+
 class Source_Proxies(models.Model):
     id = models.AutoField(primary_key=True, max_length=10)
     source_id = models.IntegerField(null=True, blank=True)
@@ -18,24 +18,6 @@ class Source_Proxies(models.Model):
     uncalled = models.IntegerField(null=True, blank=True)
     reset_at = models.DateTimeField(null=True, blank=True)
     empty_api_responses_num = models.IntegerField(null=True, blank=True)
-
-    class Meta:
-        app_label = 'proxies_service'
-        managed = False
-        db_table = 'source_proxies'
-"""
-
-class Source_Proxies(models.Model):
-    id = models.AutoField(primary_key=True, max_length=10)
-    proxy_id = models.IntegerField(null=True, blank=True)
-    source_id = models.IntegerField(null=True, blank=True)
-    used = models.IntegerField(null=True, blank=True)
-    cycles_made = models.IntegerField(null=True, blank=True)
-    active = models.BooleanField(blank=True)
-    used_at = models.DateTimeField(null=True, blank=True)
-    used_at = models.DateTimeField(null=True, blank=True)
-    reset_at = models.DateTimeField(null=True, blank=True)
-    flag_empty_api = models.IntegerField(null=True, blank=True)
 
     class Meta:
         app_label = 'proxies_service'
